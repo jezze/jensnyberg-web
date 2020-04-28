45a145931eb8c3e52c7b617d707710725c04a517
Jens Nyberg <jens.nyberg@gmail.com>
2020-04-29 00:05:46 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- wire/script.js
+++ wire/script.js
@@ -1,43 +1,37 @@
-var container;
-var camera, scene;
-var renderer;
-var wfobject;
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var scene = new THREE.Scene();
+var renderer = new THREE.WebGLRenderer();
+var geometry = new THREE.TorusKnotGeometry(10, 3, 128, 32, 2, 3, 1);
+var material = new THREE.MeshBasicMaterial({color: 0x686858, wireframe: true, transparent: true, opacity: 0.1});
+var wfobject = new THREE.Mesh(geometry, material);
 
-init();
-animate();
-
-function init() {
-    container = document.getElementById("container");
-    scene = new THREE.Scene();
-    scene.fog = new THREE.Fog(0xe8e8d8, 2000, 10000);
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(scene.fog.color);
-    container.appendChild(renderer.domElement);
-    var geometry = new THREE.TorusKnotGeometry(10, 3, 128, 32, 2, 3, 1);
-    var material = new THREE.MeshBasicMaterial({color: 0x686858, wireframe: true, transparent: true, opacity: 0.1});
-    wfobject = new THREE.Mesh(geometry, material);
-    scene.add(wfobject);
-    camera.position.z = 5;
-    window.addEventListener("resize", onWindowResize, false);
-}
-
-function animate () {
-    requestAnimationFrame( animate );
-    render();
-}
-
-function render () {
-    var r = (10 * Math.sin(Date.now()*0.0001))+10;
-    camera.position.z = r;
+function animate() {
+    camera.position.z = (10 * Math.sin(Date.now() * 0.0001)) + 10;
     wfobject.rotation.x += 0.005;
     wfobject.rotation.y += 0.005;
+
+    requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
 
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+function onload() {
+    var container = document.getElementById("container");
+
+    scene.fog = new THREE.Fog(0xe8e8d8, 2000, 10000);
+
+    scene.add(wfobject);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(scene.fog.color);
+    container.appendChild(renderer.domElement);
+    animate();
 }
+
+function onresize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("load", onload, false);
+window.addEventListener("resize", onresize, false);
